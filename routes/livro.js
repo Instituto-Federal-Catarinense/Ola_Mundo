1c3a61fc055e5353a19be47ec376934c1cac919a
SameDev <conradtsamuel@gmail.com>
2024-06-27 16:21:47 -0300

    Versão 1.0.1

--- routes/livro.js
+++ routes/livro.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'IFC - Campus' });
+  res.send('Aqui virão livros');
 });
 
 module.exports = router;
