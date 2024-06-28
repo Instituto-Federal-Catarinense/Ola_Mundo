532ce91b5fb9fcfff346306093126dfb59be3f77
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-06-28 14:59:56 -0300

    atualização aguirre

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var esportesRouter = require('./esportes/index');
+var esportesRouter = require('./routes/esportes');
 var usersRouter = require('./routes/users');
 
 var app = express();
--- routes/esportes.js
+++ routes/esportes.js
@@ -2,23 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/esportes', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
-
-module.exports = router;
 
 router.get('/boxe', function(req, res, next) {
     res.send('aba do boxe');
   });
   
-  module.exports = router;
-
   router.get('/futebol', function(req, res, next) {
     res.send('aba do futebol');
   });
-  
-  module.exports = router;
 
   router.get('/surfe', function(req, res, next) {
     res.send('aba do surfe');
