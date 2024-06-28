013809077fa1aa8f92b888564163156473263a33
Yuri Gabriel <ss.ygabriel2007@gmail.com>
2024-06-28 15:16:27 -0300

    primeira versao

---
+++ routes/esportes.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('voce esta na pagina de esportes');
+});
+
+
+router.get('/basquete', function(req, res, next) {
+  res.send('voce esta na pagina de basquete');
+});
+
+router.get('/futebol', function(req, res, next) {
+  res.send('voce esta na pagina de futebol');
+});
+
+router.get('/volei', function(req, res, next) {
+  res.send('voce esta na pagina de volei');
+});
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('voce esta na pagina de esportes');
+});
+
+router.get('/esportes', function(req, res, next) {
+  res.send('voce esta na pagina de esportes');
+});
+
+router.get('/basquete', function(req, res, next) {
+  res.send('voce esta na pagina de basquete');
+});
+
+router.get('/futebol', function(req, res, next) {
+  res.send('voce esta na pagina de futebol');
+});
+
+router.get('/volei', function(req, res, next) {
+  res.send('voce esta na pagina de volei');
 });
 
 module.exports = router;
