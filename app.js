6efbf0ef9ecf3610dd5154df6539d910a433d229
bell <mrzbldefreitas@gmail.com>
2024-06-28 14:43:15 -0300

    1.0.0.1

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var esportesRouter = require('./routes/esportes');
+var usersRouter = require('./routes/users');
 
 var app = express();
 
