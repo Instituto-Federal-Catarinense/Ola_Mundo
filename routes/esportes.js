6cdd9b0b37a6c8f58b978bbd60681a0203b2a582
bell <mrzbldefreitas@gmail.com>
2024-06-28 15:05:41 -0300

    1.1.1.1

--- routes/esportes.js
+++ routes/esportes.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.send('esportes');
 });
 
+router.get('/futebol', function(req, res, next) {
+  res.send('futebol');
+});
+
+router.get('/boxe', function(req, res, next) {
+  res.send('boxe');
+});
+
+router.get('/natacao', function(req, res, next) {
+  res.send('natação');
+});
+
+router.get('/ciclismo', function(req, res, next) {
+  res.send('ciclismo');
+});
+
+
 module.exports = router;
