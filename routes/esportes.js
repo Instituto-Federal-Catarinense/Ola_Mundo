cbd5c1570d2837e38d7fa90a1fea21f5e097a4f4
Gabriel Raupp <gabiraupppimentel@gmail.com>
2024-06-28 12:03:07 -0300

    versão_2

---
+++ routes/esportes.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('esporte');
+});
+
+router.get('/futebol', function(req, res, next) {
+  res.send('futebol');
+});
+
+router.get('/boxe', function(req, res, next) {
+  res.send('boxe');
+});
+
+router.get('/surf', function(req, res, next) {
+  res.send('surf');
+});
+
+
+module.exports = router;
