1e080cca835654da5459a161306cc1036808daeb
Zeus <itzzeusrexx@gmail.com>
2024-06-29 20:38:21 -0300

    V 1.0

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'o homem do futuro' });
+  res.render('index', { title: 'tela inicial' });
 });
 
 module.exports = router;
