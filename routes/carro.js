475197e21721618eea81187bbe639d5b14a68194
Eduardo Quartiero Marangon <eduardoqmarangon@gmail.com>
2024-07-04 16:53:52 -0300

    Versão de view

--- routes/carro.js
+++ routes/carro.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* Carro. */
 router.get('/', function(req, res, next) {
-  res.send('Carro');
+  res.send('Carro', {
+    title: 'Página de Carros',
+    imgUrl: './fotos/formula1/ayrtonsenna.jpg'
+  });
 });
+
+
 
 module.exports = router;
