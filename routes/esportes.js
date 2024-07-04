b98210bfd7d57ad2d1ce846934fd04e5910c444a
fernando <fernandoalbinoo11@gmail.com>
2024-07-04 16:31:59 -0300

    inter

--- routes/esportes.js
+++ routes/esportes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("fernando esportes");
+  res.render("esportes");
 });
 
 router.get('/futebol', function(req, res, next) {
