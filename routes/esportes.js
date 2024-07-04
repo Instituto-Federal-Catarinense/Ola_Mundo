d3cf46c20a30ff2f8e11bb603ba695042d0e049c
michell-valencio <michelvalencio099@gmai.com>
2024-07-04 16:48:22 -0300

    segundaversao

--- routes/esportes.js
+++ routes/esportes.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+  res.render('esportes', { title: 'esporte' });
+});
+
 /* GET users listing. */
 router.get('/volei', function(req, res, next) {
   res.send('respond with a resource');
