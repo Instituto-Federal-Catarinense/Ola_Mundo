e09ea4dd3016bde7454471eece7e6c561d0e0096
Eduardo Ferraz Alves <dudu.ferraz.alves@gmail.com>
2024-07-04 14:51:28 -0300

    V 1.05

---
+++ routes/esportes.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function (req, res, next) {
+    res.send('aba de esportes');
+});
+
+router.get('/surf', function (req, res, next) {
+    res.send('aba de esportes de surf');
+});
+
+router.get('/futebol', function (req, res, next) {
+    res.send('aba de esportes de futebol');
+});
+
+router.get('/volei', function (req, res, next) {
+    res.send('aba de esportes de volei');
+});
+
+router.get('/natacao', function (req, res, next) {
+    res.send('aba de esportes de natação');
+});
+
+
+
+module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Gabriela' });
+  res.render('index', { title: 'o homem do futuro' });
 });
 
 module.exports = router;
