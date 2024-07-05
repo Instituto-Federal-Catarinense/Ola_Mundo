4d4580467716994718043caa4a381853edf95307
dani980boo <daniel980boo@gmail.com>
2024-07-05 16:18:35 -0300

    murilada

--- routes/esportes.js
+++ routes/esportes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Esportes' });
+  res.render('esporte');
 });
 router.get('/futebol', function(req, res, next) {
     res.render('index', { title: 'Futebol' });
