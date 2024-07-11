dd62480dfd2303c1a0a820095c6f0d0770117a01
ZeusExx <nicolasouzaxx@gmail.com>
2024-07-11 15:11:09 -0300

    V1.8

--- routes/esportes.js
+++ routes/esportes.js
@@ -3,7 +3,32 @@ var router = express.Router();
 
 /* GET página principal de esportes. */
 router.get('/', function (req, res, next) {
-    res.render('esportes');
+    res.render('esportes', { title: 'Esportes' });
+});
+
+/* GET páginas específicas de cada esporte. */
+router.get('/volei', function (req, res, next) {
+    res.render('volei', { title: 'Vôlei' });
+});
+
+router.get('/futebol', function (req, res, next) {
+    res.render('futebol', { title: 'Futebol' });
+});
+
+router.get('/basquete', function (req, res, next) {
+    res.render('basquete', { title: 'Basquete' });
+});
+
+router.get('/ping-pong', function (req, res, next) {
+    res.render('ping-pong', { title: 'Ping-Pong' });
+});
+
+router.get('/golf', function (req, res, next) {
+    res.render('golf', { title: 'Golf' });
+});
+
+router.get('/natacao', function (req, res, next) {
+    res.render('natacao', { title: 'Natação' });
 });
 
 module.exports = router;
