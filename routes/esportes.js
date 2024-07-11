357ce748179cec23ebcebea74e4fcada3cdf34eb
emanoelbaltazar <emanoelbaltazar.nenel@gmail.com>
2024-07-11 14:56:13 -0300

    aula1107

--- routes/esportes.js
+++ routes/esportes.js
@@ -3,18 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.send('Voce esta na pagina de esportes');
+    res.render('esportes');
 });
 router.get('/volei', function(req, res, next) {
-    res.send('pagina de volei');
+    res.render('volei');
 });
 
 router.get('/futebol', function(req, res, next) {
-    res.send('pagina de futebol');
+    res.render('futebol');
 });
 
 router.get('/handball', function(req, res, next) {
-    res.send('pagina de handball');
+    res.render('handball');
+});
+
+router.get('/basquete', function(req, res, next) {
+    res.render('basquete');
 });
 
 module.exports = router;
