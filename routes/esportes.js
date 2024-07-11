4c87cd95c9df1cba9669264168d3870a8440dc01
nicolylinda <mrzbldefreitas@gmail.com>
2024-07-11 11:56:03 -0300

    testezinho

--- routes/esportes.js
+++ routes/esportes.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Página de esportes.');
+  res.render('esportes');
 });
 
 router.get('/futebol', function(req, res, next) {
-    res.send('Página de futebol.');
+    res.render('Página de futebol.');
   });
 
   router.get('/volei', function(req, res, next) {
-    res.send('Página de volei.');
+    res.render('Página de volei.');
   });
   
   router.get('/basquete', function(req, res, next) {
-    res.send('Página de basquete.');
+    res.render('Página de basquete.');
   });  
 
   router.get('/xadrez', function(req, res, next) {
-    res.send('Página de xadrez.');
+    res.render('Página de xadrez.');
   });
 
 module.exports = router;
