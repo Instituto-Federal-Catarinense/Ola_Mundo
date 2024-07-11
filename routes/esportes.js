64a13abbbf027c60ad49b86f91f86fa513b7310a
caio <bbasgvd123@gmail.com>
2024-07-11 15:29:10 -0300

    02

--- routes/esportes.js
+++ routes/esportes.js
@@ -1,20 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-router.get('/futebol', function(req, res, next) {
-  res.send('Aqui é a página de futebol');
+    res.render('esportes');
 });
 router.get('/volei', function(req, res, next) {
-  res.send('Aqui é a página de vôlei');
+    res.render('volei');
 });
+
+router.get('/futebol', function(req, res, next) {
+    res.render('futebol');
+});
+
+router.get('/handball', function(req, res, next) {
+    res.render('handball');
+});
+
 router.get('/basquete', function(req, res, next) {
-  res.send('Aqui é a página de basquete');
+    res.render('basquete');
 });
-router.get('/futsal', function(req, res, next) {
-  res.send('Aqui é a página de futsal');
-});
+
 module.exports = router;
