fbf355a1b3a3c6030e3e6cc872cd1e183148e35b
Samuca_BR <sadetona3000@gmail.com>
2024-07-11 16:36:43 -0300

    encremento nos esportres

--- routes/esportes.js
+++ routes/esportes.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('Esportes');
-  });
+  res.send('Esportes');
+});
 
 router.get('/futebol', function(req, res, next) {
-  res.send('futebas');
+  res.render('futebol');  // Renderiza a página Pug para Futebol
 });
 
 router.get('/sinuca', function(req, res, next) {
-    res.send('8pool');
-  });
+  res.render('sinuca');  // Renderiza a página Pug para Sinuca
+});
 
-  router.get('/volei', function(req, res, next) {
-    res.send('Voleizada');
-  });
-  
+router.get('/volei', function(req, res, next) {
+  res.render('volei');  // Renderiza a página Pug para Vôlei
+});
+
 module.exports = router;
