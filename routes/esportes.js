6ab76f24ef568a1d70c3123d4c70b50475417909
fernando <fernandoalbinoo11@gmail.com>
2024-07-11 16:47:01 -0300

    deu errado vo arruma

--- routes/esportes.js
+++ routes/esportes.js
@@ -3,19 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("esportes");
+  res.render('esportes', { title: "Esportes" });
 });
 
 router.get('/futebol', function(req, res, next) {
-    res.send("fernando esportes");
-  });
+  res.render('futebol', { title: "Futebol" });
+});
 
-  router.get('/boxe', function(req, res, next) {
-    res.send("fernando esportes");
-  });
+router.get('/ufc', function(req, res, next) {
+  res.render('ufc', { title: "UFC" });
+});
 
-  router.get('/surfe', function(req, res, next) {
-    res.send("fernando esportes");
-  });
+router.get('/surfe', function(req, res, next) {
+  res.render('surfe', { title: "Surfe" });
+});
 
 module.exports = router;
