872aaecc5a150dceaa9fc374dd1f8bfd1a5d1944
Gustavo de oliveira Maciel <guguinha.oliveira80@gmail.com>
2024-07-11 16:42:32 -0300

    funciona por favor

--- routes/esportes.js
+++ routes/esportes.js
@@ -1,21 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Página de esportes.');
+  res.send('Esportes');
 });
 
 router.get('/futebol', function(req, res, next) {
-    res.send('Página de futebol.');
-  });
+  res.render('futebol');  // Renderiza a página Pug para Futebol
+});
 
-  router.get('/volei', function(req, res, next) {
-    res.send('Página de volei.');
-  });
-  
-  router.get('/basquete', function(req, res, next) {
-    res.send('Página de basquete.');
-  });
+router.get('/sinuca', function(req, res, next) {
+  res.render('sinuca');  // Renderiza a página Pug para Sinuca
+});
+
+router.get('/volei', function(req, res, next) {
+  res.render('volei');  // Renderiza a página Pug para Vôlei
+});
 
 module.exports = router;
