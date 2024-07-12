d3c698332020fb23be600f9f3348ace0c3e28f6d
Yago <yagofranciscomodelsebastiao@gmail.com>
2024-07-12 15:54:30 -0300

    only

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var esportes = require('./routes/esportes');
+var esporteRouter = require('./routes/esportes');
 
 var app = express();
 
@@ -22,7 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/esportes', esportes);
+app.use('/esportes', esporteRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/esportes.js
+++ routes/esportes.js
@@ -1,22 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('esportes');
 });
-router.get('volei', function(req, res, next) {
-    res.send('Essa é a página de vôlei');
-  });
-  router.get('/futsal', function(req, res, next) {
-    res.send('Essa é a página de futsal');
-  });
-  router.get('/futebol', function(req, res, next) {
-    res.send('Essa é a página de futebol');
-  });
-  router.get('/handebol', function(req, res, next) {
-    res.send('Essa é a página de handebol');
-  });
+router.get('/volei', function(req, res, next) {
+    res.render('volei');
+});
 
+router.get('/futebol', function(req, res, next) {
+    res.render('futebol');
+});
+
+router.get('/handball', function(req, res, next) {
+    res.render('handball');
+});
+
+router.get('/basquete', function(req, res, next) {
+    res.render('basquete');
+});
 
 module.exports = router;
