779e6f106850534715dcd43c7cb16b16255d31bc
Eduardoqm27 <eduardoqm27@gmail.com>
2024-07-12 11:58:24 -0300

    Versão corrigida

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var app = express();
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var livroRouter = require('./routes/livro');
+var carroRouter = require('./routes/carro');
 
 var app = express();
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+
+// Rota para renderizar carro.pug
+app.get('/carro', (req, res) => {
+  res.render('carro', { title: 'Informações sobre Carros' }); // Passa o título como variável para o template
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/livros', livroRouter);
+app.use('/carro', carroRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
