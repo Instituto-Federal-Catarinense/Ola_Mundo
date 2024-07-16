e54da2e018cffb454634c243e04f9b2ff2d63ec0
bell <mrzbldefreitas@gmail.com>
2024-07-16 16:33:40 -0300

    alteração ejs

--- app.js
+++ app.js
@@ -12,8 +12,11 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+// Configurar o caminho das views (se necessário)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+
+// Configurar o mecanismo de visualização para EJS
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
