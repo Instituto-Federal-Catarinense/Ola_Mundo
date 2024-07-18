cbd50ab00f177cda9b890b3f0f791898a8246b0d
bell <mrzbldefreitas@gmail.com>
2024-07-18 11:56:09 -0300

    Versão Views novas

--- routes/esportes.js
+++ routes/esportes.js
@@ -1,25 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET sports page. */
 router.get('/', function(req, res, next) {
   res.render('esportes');
 });
 
-router.get('/Volêi', function(req, res, next) {
-    res.send('Página de Volêi');
-  });
-  
-  router.get('/Volêi de areia', function(req, res, next) {
-    res.send('Página de Baseball.');
-  });
-  
-  router.get('/Basquete', function(req, res, next) {
-    res.send('Página de Basquete.');
-  });
-  
-  router.get('/Futebol', function(req, res, next) {
-    res.send('Página de Futebol');
-  });
+router.get('/volei', function(req, res, next) {
+  res.send('Página de Vôlei');
+});
+
+router.get('/volei-de-areia', function(req, res, next) {
+  res.send('Página de Vôlei de Areia');
+});
+
+router.get('/basquete', function(req, res, next) {
+  res.send('Página de Basquete');
+});
+
+router.get('/futebol', function(req, res, next) {
+  res.send('Página de Futebol');
+});
+
+router.get('/futebol-de-salao', function(req, res, next) {
+  res.send('Página de Futebol de Salão');
+});
 
 module.exports = router;
