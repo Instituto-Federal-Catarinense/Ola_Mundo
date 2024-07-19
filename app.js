aef42e449d3e607a5270b6cadc1a64ba33e28451
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-07-19 16:19:55 -0300

    jvjjdgvikfvju

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var esporteRouter = require('./routes/esportes');
+var musicaRouter = require('./routes/musica');
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/esportes', esporteRouter);
-
+app.use('/musica', musicaRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
