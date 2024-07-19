5c43195f571b2ae45d5f230bef63d0e8ebd6d79f
colleoni <isabeliccolleoni@gmail.com>
2024-07-19 16:12:06 -0300

    versao com a rota esportes

--- app.js
+++ app.js
@@ -1,43 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var esportesRouter = require('./routes/esportes');
+const app = express();
 
-var app = express();
+// Configuração para o uso de Pug como template engine
+app.set('views', path.join(__dirname, 'views')); // Define o diretório onde os arquivos de template estão localizados
+app.set('view engine', 'pug'); // Define o template engine como Pug
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// Middleware para servir arquivos estáticos (opcional)
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/esportes', esportesRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// Exemplo de rota para renderizar uma página
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Página Inicial', message: 'Bem-vindo ao meu site!' });
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// Exemplo de rota para renderizar outra página
+app.get('/sobre', function(req, res) {
+  res.render('sobre', { title: 'Sobre', message: 'Conheça mais sobre nossa empresa.' });
 });
 
-module.exports = app;
+// Porta do servidor
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
--- routes/esportes.js
+++ routes/esportes.js
@@ -1,24 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Você está na página de esportes!');
+  res.render('index', { title: 'Página Inicial' });
 });
 
 router.get('/surf', function(req, res, next) {
-    res.send('Você está na página de surf!');
-  });
+  res.render('surf', { title: 'Surf' });
+});
 
-  router.get('/motocross', function(req, res, next) {
-    res.send('Você está na página de motocross!');
-  });
+router.get('/futebol', function(req, res, next) {
+  res.render('futebol', { title: 'Futebol' });
+});
 
-  router.get('/futebol', function(req, res, next) {
-    res.send('Você está na página de futebol!');
-  });
-  router.get('/volei', function(req, res, next) {
-    res.send('Você está na página de volei!');
-  });
+router.get('/volei', function(req, res, next) {
+  res.render('volei', { title: 'Vôlei' });
+});
+
+router.get('/basquete', function(req, res, next) {
+  res.render('basquete', { title: 'Basquete' });
+});
+
+router.get('/handebol', function(req, res, next) {
+  res.render('handebol', { title: 'Handebol' });
+});
+
+router.get('/subrota1', function(req, res, next) {
+  res.render('subrota1', { title: 'Subrota1' });
+});
+
+router.get('/subrota2', function(req, res, next) {
+  res.render('subrota2', { title: 'Subrota2' });
+});
+
+router.get('/subrota3', function(req, res, next) {
+  res.render('subrota3', { title: 'Subrota3' });
+});
+
+router.get('/subrota4', function(req, res, next) {
+  res.render('subrota4', { title: 'Subrota4' });
+});
+
+router.get('/subrota5', function(req, res, next) {
+  res.render('subrota5', { title: 'Subrota5' });
+});
+
 
 module.exports = router;
