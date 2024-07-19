576b2faa89f89b2f33a6e5866fea6d9d4721ec82
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-07-19 16:01:00 -0300

    msuicas

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'jodio' });
+  res.render('index', { title: 'The Man of the World' });
 });
 
 module.exports = router;
