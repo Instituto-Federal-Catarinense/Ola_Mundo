a64f6ea2d687eef866a24d712f0e73ddea1b7e4f
fernando <fernandoalbinoo11@gmail.com>
2024-07-19 14:57:42 -0300

    volta pra .pug

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var esportesRouter = require('./routes/esportes');
 
+
 var app = express();
 
-// Configuração do EJS como engine de template
-app.set('views', path.join(__dirname, 'views')); // Define o diretório onde estão os templates
-app.set('view engine', 'ejs'); // Define EJS como view engine
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,20 +25,21 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/esportes', esportesRouter);
 
-// Captura de erro 404 e encaminhamento para o manipulador de erros
+
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// Manipulador de erros
+// error handler
 app.use(function(err, req, res, next) {
-  // Define variáveis locais, fornecendo erro apenas em desenvolvimento
+  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // Renderiza a página de erro usando o template 'error.ejs'
+  // render the error page
   res.status(err.status || 500);
-  res.render('error'); 
+  res.render('error');
 });
 
 module.exports = app;
