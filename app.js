2552848e9f3d18d3681adffacabd5d1d0adde806
pires <joaopiresmachadonunes@gmail.com>
2024-07-19 16:50:55 -0300

    versão 1.0.6

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mustacheExpress = require('mustache-express'); // Importar o mustache-express
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,9 +12,10 @@ var gamesRouter = require('./routes/games');
 
 var app = express();
 
-// view engine setup
+// Configuração do mecanismo de visualização Mustache
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -21,25 +23,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rotas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/esportes', sportsRouter);
 app.use('/games', gamesRouter);
 
-// catch 404 and forward to error handler
+// Middleware para lidar com erro 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Middleware de tratamento de erro
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
+  // Definir variáveis locais para a renderização de erro
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
+  // Renderizar a página de erro
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error'); // Renderiza o template de erro (error.mustache)
 });
 
 module.exports = app;
