95667323229050472cb8befa4e93d75c9c582cf0
Redes IFC <redes@lab32-pc01.sombrio.ifc.edu.br>
2024-07-19 15:20:00 -0300

    arrumar

--- routes/esportes.js
+++ routes/esportes.js
@@ -6,20 +6,20 @@ router.get('/', function(req, res, next) {
     res.render('esportes', { title: 'Lista de melhores esportes' });
 });
 
-router.get('/Volei', function(req, res, next) {
-    res.send('Primeiro: volei');
+router.get('/volei', function(req, res, next) {
+    res.send('volei');
 });
 
-router.get('/Volei de areia', function(req, res, next) {
-    res.send('Segundo: volei de areia');
+router.get('/volei-de-areia', function(req, res, next) {
+    res.send('volei de areia');
 });
 
-router.get('/Basquete', function(req, res, next) {
-    res.send('Terceiro: basquete');
+router.get('/basquete', function(req, res, next) {
+    res.send('basquete');
 });
 
-router.get('/Futebol', function(req, res, next) {
-    res.send('Quarto: futebol');
+router.get('/futebol', function(req, res, next) {
+    res.send('futebol');
 });
 
 module.exports = router;
