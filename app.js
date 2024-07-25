b49fcaec7c9766e2a915da17784a7d22507412da
murilorochars <ofcsmurilo@gmail.com>
2024-07-25 13:38:29 -0300

    versao com um arquivo mustache

--- app.js
+++ app.js
@@ -3,17 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mustacheExpress = require('express-mustache')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var EsporteRouter = require('./routes/esportes');
 var Pokemon = require('./routes/pokemon')
 
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.engine('mustache', mustacheExpress());
+app.set('view engine', 'mustache');
+
 
 app.use(logger('dev'));
 app.use(express.json());
