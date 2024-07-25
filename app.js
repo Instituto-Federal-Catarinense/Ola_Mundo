53c5dd2cac97c321143544312c506b42efb84b03
Caio Pietsch <bbasgvd123@gmail.com>
2024-07-25 15:13:21 -0300

    02

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mustacheExpress = require('mustache-express');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var esporteRouter = require('./routes/esportes');
+var esportesRouter = require('./routes/esportes');
+var musicasRouter = require('./routes/musicas'); // Adicionando o roteador de músicas
 
 var app = express();
 
 // view engine setup
+app.engine('mustache', mustacheExpress());
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set('view engine', 'mustache');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -22,8 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/esportes', esporteRouter);
-
+app.use('/esportes', esportesRouter);
+app.use('/musicas', musicasRouter); // Adicionando a nova rota de músicas
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -38,7 +41,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', { message: err.message, error: err });
 });
 
 module.exports = app;
