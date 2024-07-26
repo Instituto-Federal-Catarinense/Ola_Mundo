20f8f3c971762f69dea55d79869282baa820b975
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2024-07-26 15:11:38 -0300

    versao pronta

--- app.js
+++ app.js
@@ -3,40 +3,39 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mustacheExpress = require('express-mustache')
+var mustacheExpress = require('mustache-express')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var EsporteRouter = require('./routes/esportes');
-var Pokemon = require('./routes/pokemon')
-
+var PokemonRouter = require('./routes/pokemon'); // Renomeei para PokemonRouter para ser mais claro
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 app.engine('mustache', mustacheExpress());
-app.set('view engine', 'mustache');
+app.set('view engine', 'mustache'); // Aqui você define o mecanismo de template para mustache
 
-
+// Middlewares
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Rotas
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/esportes', EsporteRouter);
-app.use('/pokemon', Pokemon);
+app.use('/pokemon', PokemonRouter); // Use PokemonRouter para a rota /pokemon
 
-// catch 404 and forward to error handler
+// Middleware para lidar com erro 404
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+// Middleware de tratamento de erro
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
